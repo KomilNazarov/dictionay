2242be99cb6e69134bb37802281bf0baf6fc6fa1
komilnazarov <nkomil123@gamil.com>
2021-11-11 01:40:11 +0500

    ADD SOMETHING

--- dictionay/settings.py
+++ dictionay/settings.py
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'dictionay.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dictionary',
-#         'USER': 'postgres',
-#         'PASSWORD': 123,
-#         'HOST': 'localhost',
-#         'PORT': 5432,
-#
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dictionary',
+        'USER': 'postgres',
+        'PASSWORD': 123,
+        'HOST': 'localhost',
+        'PORT': 5432,
+
+    }
+}
 
 
 # Password validation
@@ -137,7 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, "/static/")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
